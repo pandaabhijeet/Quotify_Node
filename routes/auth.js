bddce47f711f180ec2a8deac33d4ac77287d6335
chronicle <abhijeetpanda06@gmail.com>
2022-10-01 08:43:30 +0530

    bcrypt-fix

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const userModel = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const {registerValidation, loginValidation} = require('../models/validation');
 const jwt = require('jsonwebtoken');
 
