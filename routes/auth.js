165970bba48523da200179f64a6d71eced81a873
chronicle <abhijeetpanda06@gmail.com>
2022-10-08 08:29:53 +0530

    debug

--- routes/auth.js
+++ routes/auth.js
@@ -49,8 +49,10 @@ router.post('/register', async (req,res) => {
         const savedUser = await user.save();
         console.log(savedUser);
         res.send(savedUser);
+        res.send('Success : Sign Up Successfull');
         }catch(err){
         res.status(400).send(err);
+        res.send('err : Error');
         console.log(err);
         }
      }
