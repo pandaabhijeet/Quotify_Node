76c68cf9344167ec750cefe86d85d349c7ad554c
chronicle <abhijeetpanda06@gmail.com>
2022-11-21 08:43:44 +0530

    improve

--- models/imagemodel.js
+++ models/imagemodel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ImageSchema = mongoose.Schema({
     image : {
-        data : Buffer,
+        data : ImageBitmap,
         contentType : String
     }
 });
